074260899da24f703e9705d969eacf45aa2f1d54
Marina Oliveira <marinaoliveira.r07@gmail.com>
2020-10-10 18:48:06 -0300

    website complete!

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// modal
 let modalBtn = document.querySelector(".modal-btn");
 let modalBg = document.querySelector(".modal-bg");
 let modalX = document.querySelector(".modal-x");
@@ -9,3 +10,18 @@ modalBtn.addEventListener("click", () => {
 modalX.addEventListener("click", () => {
   modalBg.classList.remove("modal-active");
 });
+
+// menu
+let menu = document.querySelector(".menu");
+let navLinks = document.querySelector(".nav-links");
+let links = document.querySelectorAll(".nav-links li");
+
+menu.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+});
+
+for (const link of links) {
+  link.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+  });
+}
